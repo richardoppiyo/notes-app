c9789b8e5e160fad2bdcd2fbca0ff15196afc948
Richard Opiyo <romenyo@dewcis.com>
2021-12-03 10:09:06 +0300

    new changes

--- notes.js
+++ notes.js
@@ -1,9 +1,39 @@
-
+const fs = require("fs");
 
 module.exports.getNotes = function(){
     return ("Your notes  ........");
 };
 
-module.exports.addNotes = function(title,body){
+module.exports.addNotes = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
 
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+
+
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJSON);
+}
+
+const loadNotes =  function(){
+    try{
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    }catch(e){
+    return [];
+    }
 }
